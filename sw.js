2d9308dddb809e31d37e171e70ae4076ddf8ca1d
mmaier90-afk <m.maier90@gmail.com>
2026-06-11 19:37:11 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='gymm-buddy-time-dashboard-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./logo-192.png','./logo-512.png','./logo-full.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
