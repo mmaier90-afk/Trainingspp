5fcedeab808a99fd9219763fe0dd5a01e31ed811
mmaier90-afk <m.maier90@gmail.com>
2026-06-11 19:37:28 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='gymm-buddy-required-set-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./logo-192.png','./logo-512.png','./logo-full.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
